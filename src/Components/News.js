034d376c1b451011c97891270cfe981664da1626
Arjun Patel <hackmech007@gmail.com>
2023-06-20 17:48:59 +0530

    impliment infiinite scrolling

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 export default function News(props) {
@@ -14,32 +15,35 @@ export default function News(props) {
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
+    // const [totalPages, setTotalPages] = useState(0);
+    const [totalResults, setTotalResults] = useState(0);
 
-    const updateNews = async ()=> {
-        setLoading(true);
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=ac20c74289024f23859d8e31639e0545&pageSize=${props.pageSize}&page=${page}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        setArticles(data.articles);
-        setLoading(false);
-    }
+    // const updateNews = async () => {
+    //     setLoading(true);
+    //     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=3aa85b8e1906432bbf4b9bdebe9f3d04&pageSize=${props.pageSize}&page=${page}`;
+    //     const response = await fetch(url);
+    //     const data = await response.json();
+    //     console.log(data);
+    //     setArticles(data.articles);
+    //     setLoading(false);
+    // }
 
     useEffect(() => {
         const fetchArticles = async () => {
             try {
+                setPage(1);
                 setLoading(true);
-                const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=ac20c74289024f23859d8e31639e0545&pageSize=${props.pageSize}`;
+                const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=3aa85b8e1906432bbf4b9bdebe9f3d04&pageSize=${props.pageSize}&page=${page}`;
                 const response = await fetch(url);
                 const data = await response.json();
                 console.log(data);
                 setArticles(data.articles);
+                setTotalResults(data.totalResults)
                 setLoading(false);
                 console.log("effect " + page);
 
-                const totalPages = Math.ceil(data.totalResults / props.pageSize);
-                setTotalPages(totalPages);
+                // const totalPages = Math.ceil(data.totalResults / props.pageSize);
+                // setTotalPages(totalPages);
 
             } catch (error) {
                 console.error('Error fetching articles:', error);
@@ -47,58 +51,79 @@ export default function News(props) {
             }
         };
         fetchArticles();
-    }, [props.country, props.category, props.pageSize, page]);
+    }, [props.country, props.category]);
 
-    const handlePrevClick = () => {
-        // setLoading(true);
-        // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=ac20c74289024f23859d8e31639e0545&pageSize=${props.pageSize}&page=${page - 1}`;
-        // let response = await fetch(url);
-        // let data = await response.json();
-        // setArticles(data.articles)
-        // setLoading(false);
-        setPage(page - 1);
-        console.log("prev " +page);
-        updateNews();
-    }
+    // const handlePrevClick = () => {
+    //     // setLoading(true);
+    //     // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=3aa85b8e1906432bbf4b9bdebe9f3d04&pageSize=${props.pageSize}&page=${page - 1}`;
+    //     // let response = await fetch(url);
+    //     // let data = await response.json();
+    //     // setArticles(data.articles)
+    //     // setLoading(false);
+    //     setPage(page - 1);
+    //     console.log("prev " + page);
+    //     updateNews();
+    // }
 
-    const handleNextClick = () => {
-        // setLoading(true);
-        // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=ac20c74289024f23859d8e31639e0545&pageSize=${props.pageSize}&page=${page + 1}`;
-        // let response = await fetch(url);
-        // let data = await response.json();
-        // setArticles(data.articles)
-        // setLoading(false);
+    // const handleNextClick = () => {
+    //     // setLoading(true);
+    //     // let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=3aa85b8e1906432bbf4b9bdebe9f3d04&pageSize=${props.pageSize}&page=${page + 1}`;
+    //     // let response = await fetch(url);
+    //     // let data = await response.json();
+    //     // setArticles(data.articles)
+    //     // setLoading(false);
 
+    //     setPage(page + 1);
+    //     console.log("next" + page);
+    //     updateNews();
+    // }
+    const fetchMoreData = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=3aa85b8e1906432bbf4b9bdebe9f3d04 &pageSize=${props.pageSize}&page=${page+1}`;
         setPage(page + 1);
-        console.log("next" +page);
-        updateNews();
-    }
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setArticles(articles.concat(data.articles));
+        console.log("fetchmore " + page);
+    };
 
     return (
-        <div className="container my-3">
+        <>
             <h1 className="my-3 text-center">NewsMonkey - Top {props.category} Headlines</h1>
             {loading && <Spinner />}
-            <div className="row">
-                {!loading && articles.map((element) => {
-                    return (
-                        <div className="col-md-4 my-2" key={element.url}>
-                            <NewsItem
-                                title={element.title}
-                                description={element.description}
-                                imgUrl={element.urlToImage}
-                                newsUrl={element.url}
-                                author={element.author}
-                                date={element.publishedAt}
-                                source={element.source.name} />
-                        </div>
-                    )
-                })}
-            </div>
-            <div className="container d-flex justify-content-between my-2">
+
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length !== totalResults}
+                loader={<Spinner />}
+            >
+                <div className="container">
+                    <div className="row">
+                        {articles.map((element) => {
+                            return (
+                                <div className="col-md-4 my-2" key={element.url}>
+                                    <NewsItem
+                                        title={element.title}
+                                        description={element.description}
+                                        imgUrl={element.urlToImage}
+                                        newsUrl={element.url}
+                                        author={element.author}
+                                        date={element.publishedAt}
+                                        source={element.source.name} />
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </InfiniteScroll>
+
+
+            {/* <div className="container d-flex justify-content-between my-2">
                 <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={handlePrevClick}>&larr; Previous</button>
                 <button disabled={page >= totalPages} type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
-            </div>
-        </div>
+            </div> */}
+        </>
     )
 }
 
