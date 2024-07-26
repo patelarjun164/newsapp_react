9a915720b430223f8432a7476e7fefd77ea68f76
Arjun Patel <hackmech007@gmail.com>
2024-07-26 16:26:42 +0530

    headline updated to newswave on home page

--- src/komponent/News.js
+++ src/komponent/News.js
@@ -84,7 +84,7 @@ export default function News(props) {
 
     return (
         <>
-            <h1 className="text-center" style={{ marginTop: "70px", marginBottom: "20px" }}>NewsMonkey - Top {props.category} Headlines</h1>
+            <h1 className="text-center" style={{ marginTop: "70px", marginBottom: "20px" }}>NewsWave - Top {props.category} Headlines</h1>
             {loading && <Spinner />}
 
             <InfiniteScroll
